5b39ed53666fa1b01e20eb56c44fe0d4e1f1158d
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-23 20:17:02 +0000

    Final copy

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const myAge = 13
+const myAge = 16
 let answer = 0
 
 document.getElementById('button').addEventListener('click', newfunction)
 function newfunction () {
-  while (myAge != answer) {
-  answer = prompt('guess my age') 
-  if (answer == myAge) {
-    alert ('You got it right!')
-} else if (answer < myAge) {
-    alert ('You guessed to small, please try again')
-} else if (answer > myAge) {
-    alert ('you guessed to big, pease try again')
-} else {
-  alert('invalid answer, please try again')
-}
-}
+  while (myAge !== answer) {
+    answer = prompt('guess my age')
+    if (answer === myAge) {
+      alert('You got it right!')
+    } else if (answer < myAge) {
+      alert('You guessed to small, please try again')
+    } else if (answer > myAge) {
+      alert('you guessed to big, pease try again')
+    } else {
+      alert('invalid answer, please try again')
+    }
+  }
 }
